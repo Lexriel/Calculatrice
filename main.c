33855059cad42e77fc0cea197542ef5f8a462ef4
Lexriel <atemperville@cesi.fr>
2017-10-26 14:42:08 +0200

    correction addition

--- main.c
+++ main.c
@@ -6,10 +6,6 @@ int addition(int a, int b){
 }
 
 int main(){
-<<<<<<< HEAD
-    printf("4+7 vaut %d\n", addition(4, 7));
-=======
-    printf("addition : 4+7 = %d\n", addition(4, 7));
->>>>>>> aae3906d1aa07a7b745e37515a4b2544db9c8593
+    printf("4+7 = %d\n", addition(4, 7));
     return 0;
 }
